eba1955bb5a926a69ae2606d4e100c2d56d5ae34
rosicleia.souza <rosicleia.souza@10.1.48.216>
2013-08-02 15:48:30 -0300

    Criando a classe Modelo.

---
+++ CrudJDBC/src/br/com/modelo/negocio/Modelo.java
@@ -0,0 +1,73 @@
+
+package br.com.modelo.negocio;
+
+/**
+ *
+ * @author rosicleia.souza
+ */
+public class Modelo {
+    private int id;
+    private String descricao;
+    private int potencia;
+    private Marca marca;
+
+    public Modelo() {
+    }  
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the potencia
+     */
+    public int getPotencia() {
+        return potencia;
+    }
+
+    /**
+     * @param potencia the potencia to set
+     */
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    /**
+     * @return the marca
+     */
+    public Marca getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
+}
